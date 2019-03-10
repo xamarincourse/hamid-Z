d5dd98c0984aed402f4865db01e1f28d4278e275
Hamid Zahedi <Hamid.Zahedi@MahanAir.Aero>
2019-03-10 17:05:32 +0330

    my source files first init

---
+++ LayoutSample/LayoutSample/UITapGestureRecognizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LayoutSample
+{
+    internal class UITapGestureRecognizer
+    {
+        private Func<object, object> p;
+
+        public UITapGestureRecognizer(Func<object, object> p)
+        {
+            this.p = p;
+        }
+
+        public int NumberOfTapsRequired { get; set; }
+    }
+}
