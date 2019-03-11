7b31936772f6a5dcd13c0366ed55fd93d8938137
Hamid Zahedi <Hamid.Zahedi@MahanAir.Aero>
2019-03-11 15:37:42 +0330

    my project files

---
+++ LayoutSample/LayoutSample/SecondPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutSample
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SecondPage : ContentPage
+	{
+
+
+        public SecondPage ()
+		{
+			InitializeComponent ();
+            var Parvaz = new Flight()
+            {
+                getArv = "SFO",
+                getDep = "LAX",
+                getTerm = "1",
+                getGate = "A5",
+                getSeat = "5A",
+                getDepart = "1:00PM",
+                getStatus="On Time"
+
+        };
+            
+
+            this.BindingContext = Parvaz;
+
+        }
+        async void OnButtonTest(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new FlightList());
+        }
+
+        async void Handle_Tapped(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new FlightList());
+        }
+    }
+}
